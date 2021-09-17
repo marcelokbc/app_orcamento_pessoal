e79ae7829fbca8517ee3bd731fd5f7f6e750ae7c
Marcelo <marcelo.cansilva@gmail.com>
2021-09-16 23:24:09 -0300

    Control and data consistency

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ class Despesa {
 		this.descricao = descricao
 		this.valor = valor
 	}
+
+	validarDados() {
+		for(let i in this){
+			if(this[i] == undefined || this[i] == '' || this[i] == null) {
+				return false
+			}
+		}
+
+		return true
+	}
 }
 
 class Bd {
@@ -54,5 +64,13 @@ function cadastrarDespesa() {
 	  valor.value
 	)
 
-	bd.gravar(despesa)
+	if(despesa.validarDados()) {
+		//bd.gravar(despesa)
+		//dialog de sucesso
+		console.log('Dados válidos')
+	} else {
+		//dialog de erro 
+		console.log('Dados inválidos')
+	}
+	
 } 
