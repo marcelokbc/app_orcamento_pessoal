064a0057da02648ff9cc7db6c75a098e6e29f129
Marcelo <marcelo.cansilva@gmail.com>
2021-09-20 23:18:52 -0300

    Add filter to search expense not yet related to bd

--- app.js
+++ app.js
@@ -65,6 +65,10 @@ class Bd {
 
 		return despesas
 	}
+
+	pesquisar(despesa) {
+		console.log(despesa)
+	}
 }
 
 let bd = new Bd()
@@ -170,6 +174,19 @@ function carregaListaDespesas() {
 
 	})
 
+}
+
+function pesquisarDespesa() {
+	let ano = document.getElementById('ano').value
+	let mes = document.getElementById('mes').value
+	let dia = document.getElementById('dia').value
+	let tipo = document.getElementById('tipo').value
+	let descricao = document.getElementById('descricao').value
+	let valor = document.getElementById('valor').value
+
+	let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+
+	bd.pesquisar(despesa)
 }
 
 
