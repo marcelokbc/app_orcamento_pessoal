e480b2e403a2e62f904434ae665eb2b1a2756244
Marcelo <marcelo.cansilva@gmail.com>
2021-09-22 00:15:24 -0300

    Add button to exclude despesas

--- app.js
+++ app.js
@@ -60,6 +60,7 @@ class Bd {
 				continue
 			}
 
+			despesa.id = i
 			despesas.push(despesa)
 		}
 
@@ -113,6 +114,10 @@ class Bd {
 		}
 
 		return despesasFiltradas
+	}
+
+	remover(id) {
+		localStorage.removeItem(id)
 	}
 }
 
@@ -191,7 +196,7 @@ function carregaListaDespesas(despesas = Array(), filtro = false) {
 	//percorrer o array despesas, listando cada despesa de forma dinâmica
 	despesas.forEach(function(d) {
 
-		console.log(d)
+		//console.log(d)
 
 		//criando a linha(tr)
 		let linha = listaDespesas.insertRow()
@@ -217,6 +222,27 @@ function carregaListaDespesas(despesas = Array(), filtro = false) {
 
 		linha.insertCell(2).innerHTML = d.descricao
 		linha.insertCell(3).innerHTML = d.valor
+
+		//criar o botão de exclusão
+		let btn = document.createElement("button")
+		btn.className = 'btn btn-danger'
+		btn.innerHTML = '<i class="fas fa-times"></i>'
+		btn.id = `id_despesa_${d.id}`
+		btn.onclick = function() { 
+			// remover a despesa
+
+			let id = this.id.replace('id_despesa_', '')
+
+			//alert(id)
+
+			bd.remover(id)
+
+			window.location.reload()
+
+		} 
+		linha.insertCell(4).append(btn)
+
+		console.log(d)
 
 
 	})
